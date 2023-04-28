9390eaa08816233e5811242ccbb46e3b37fc9038
JoeZiminski <joseph.j.ziminski@gmail.com>
2023-04-28 18:09:06 +0100

    push visualise improvements handle multi-shank

--- swc_ephys/pipeline/data_class.py
+++ swc_ephys/pipeline/data_class.py
@@ -1,3 +1,4 @@
+import fnmatch
 import os
 import pickle  # TODO: explore cPickle
 from collections import UserDict
@@ -5,6 +6,7 @@ from collections.abc import ItemsView, KeysView, ValuesView
 from pathlib import Path
 from typing import Dict, List, Optional, Tuple, Union
 
+import numpy as np
 import spikeinterface as si
 from spikeinterface.core.base import BaseExtractor
 
@@ -53,7 +55,7 @@ class Data(UserDict):
         """
         super(Data, self).__init__()
 
-        self.base_path, self.all_run_names, self.rawdata_path = self.validate_inputs(
+        self.base_path, checked_run_names, self.rawdata_path = self.validate_inputs(
             run_names, base_path
         )
 
@@ -62,8 +64,11 @@ class Data(UserDict):
         (
             self.run_level_path,
             self.all_run_paths,
+            self.all_run_names,
             self.run_name,
-        ) = self.create_runs_from_single_or_multiple_run_names()
+        ) = self.create_runs_from_single_or_multiple_run_names(checked_run_names)
+
+        utils.message_user(f"The order of the loaded runs is:" f"{self.all_run_names}")
 
         self.pp_steps = pp_steps
         self.data: Dict = {"0-raw": None}
@@ -101,22 +106,35 @@ class Data(UserDict):
                 "all" in run_names and len(run_names) != 1
             ), "'all' run name must be used on its own."
 
+        for name in run_names:
+            assert "g0" not in name.split("_"), (
+                f"gate index should not be on the run name. Remove _g0 from\n" f"{name}"
+            )
+
+        for name in run_names:  # TODO: clean_up ...
+            gate_idx_in_name = fnmatch.filter(name.split("_"), "g?")
+            assert len(gate_idx_in_name) == 0, (
+                f"Gate with index larger than 0 is not supported. This is found "
+                f"in run name {name}. "
+            )
+
         return base_path, run_names, rawdata_path
 
     # Load and Save --------------------------------------------------------------------
 
     def create_runs_from_single_or_multiple_run_names(
         self,
-    ) -> Tuple[Path, List[Path], str]:
+        run_names: List[str],
+    ) -> Tuple[Path, List[Path], List[str], str]:
         """ """
-        if self.all_run_names == ["all"] or len(self.all_run_names) > 1:
-            all_run_paths, run_name = self.get_multi_run_names_paths()
+        if run_names == ["all"] or len(run_names) > 1:
+            all_run_paths, run_name = self.get_multi_run_names_paths(run_names)
         else:
-            all_run_paths = [self.make_run_path(f"{self.all_run_names[0]}_g0")]
-            run_name = self.all_run_names[0]
+            all_run_paths = [self.make_run_path(f"{run_names[0]}_g0")]
+            run_name = run_names[0]
 
-        assert len(self.all_run_paths) > 0, (
-            f"No runs found for {self.all_run_names}. "
+        assert len(run_names) > 0, (
+            f"No runs found for {run_names}. "
             f"Make sure to specify run_names without gate / trigger (e.g. no _g0)."
         )
 
@@ -124,15 +142,20 @@ class Data(UserDict):
         utils.assert_list_of_files_are_in_datetime_order(all_run_paths)
         run_level_path = self.make_run_path(run_name)
 
-        return run_level_path, all_run_paths, run_name
+        all_run_names = [path_.stem for path_ in all_run_paths]
 
-    def get_multi_run_names_paths(self) -> Tuple[List[Path], str]:
+        return run_level_path, all_run_paths, all_run_names, run_name
+
+    def get_multi_run_names_paths(
+        self,
+        run_names: List[str],
+    ) -> Tuple[List[Path], str]:
         """ """
         search_run_paths = list(
             Path(self.base_path / "rawdata" / self.sub_name).glob("*_g0")
         )
 
-        if self.all_run_names == ["all"]:
+        if run_names == ["all"]:
             search_run_paths = utils.sort_list_of_paths_by_datetime_order(
                 search_run_paths
             )
@@ -140,19 +163,19 @@ class Data(UserDict):
                 search_run_paths, "modification"
             )
         else:
+            if run_names != ["all"]:
+                check_run_names = [f"{name}_g0" for name in run_names]
+                search_run_paths = [
+                    name for name in search_run_paths if name.stem in check_run_names
+                ]
+
             utils.assert_list_of_files_are_in_datetime_order(
                 search_run_paths, "creation"
             )
 
-        if self.all_run_names != ["all"]:
-            check_run_names = [f"{name}_g0" for name in self.all_run_names]
-            search_run_paths = [
-                name for name in search_run_paths if name.stem in check_run_names
-            ]
-
         all_run_paths = search_run_paths
 
-        run_name = self.make_run_name_from_multiple_run_names(self.all_run_names)
+        run_name = self.make_run_name_from_multiple_run_names(run_names)
 
         return all_run_paths, run_name
 
@@ -257,3 +280,16 @@ class Data(UserDict):
 
     def make_run_path(self, run_name: str) -> Path:
         return self.base_path / "rawdata" / self.sub_name / f"{run_name}"
+
+    def get_probe_group_num(self):
+        """
+        This is shank num
+
+        TODO
+        ---
+        This is getting out of scope for this class, which should really be
+        file-path related. Understand how shank index on the probe property
+        maps to real-world shank
+        """
+        num_groups = np.unique(self["0-raw"].get_property("group")).size
+        return num_groups
