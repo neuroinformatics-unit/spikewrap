64a6fa1ab1675bda69cd15e5a7c84e1307f598c3
Joseph Ziminski <jziminski@gpu-sr670-20.hpc.swc.ucl.ac.uk>
2023-05-03 15:36:57 +0000

    testing time range on visualise

--- swc_ephys/examples/example_visualise_preprocessing_output.py
+++ swc_ephys/examples/example_visualise_preprocessing_output.py
@@ -1,9 +1,7 @@
 from swc_ephys.pipeline.visualise import visualise_preprocessing_output
 
 preprocessing_path = (
-    # "/home/joe/data/derivatives/1110925/1110925_test_shank1_cut/preprocessed"
     "/home/joe/data/steve_multi_run/derivatives/1119617/all/preprocessed"
-    # r"/home/joe/data/derivatives/1110925/1110925_test_shank1_cut/preprocessed"
 )
 
 visualise_preprocessing_output(
