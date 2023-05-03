57ed5deabe67719194d912f73062f38ae6e3fe25
JoeZiminski <joseph.j.ziminski@gmail.com>
2023-05-03 21:48:55 +0100

    Handle where sorter images are stored locally if not on HPC.

--- swc_ephys/examples/example_full_pipeline.py
+++ swc_ephys/examples/example_full_pipeline.py
@@ -3,10 +3,11 @@ from pathlib import Path
 from swc_ephys.pipeline.full_pipeline import run_full_pipeline
 
 base_path = Path(
-    r"/ceph/neuroinformatics/neuroinformatics/scratch/jziminski/ephys/test_data/steve_multi_run/1119617"
-    r"/time-short_samenaming"
+    "/home/joe/data/steve_multi_run"
+    #    r"/ceph/neuroinformatics/neuroinformatics/scratch/jziminski/ephys/test_data/steve_multi_run/1119617"
+    #   r"/time-short_samenaming"
 )
-sub_name = "cut_same_name"
+sub_name = "1119617"  # "cut_same_name"
 run_names = "all"
 
 config_name = "test"
@@ -19,7 +20,7 @@ if __name__ == "__main__":
         run_names,
         config_name,
         sorter,
-        use_existing_preprocessed_file=False,
+        use_existing_preprocessed_file=True,
         overwrite_existing_sorter_output=True,
         slurm_batch=False,
     )
