120fb7e11817db926472814ba831c044accd889f
JoeZiminski <joseph.j.ziminski@gmail.com>
2023-05-03 22:07:21 +0100

    improve handling of run datetime order

--- swc_ephys/pipeline/full_pipeline.py
+++ swc_ephys/pipeline/full_pipeline.py
@@ -1,6 +1,6 @@
 import copy
 from pathlib import Path
-from typing import Union
+from typing import List, Union
 
 from ..configs.configs import get_configs
 from ..utils import slurm, utils
@@ -13,7 +13,7 @@ from .sort import run_sorting
 def run_full_pipeline(
     base_path: Union[Path, str],
     sub_name: str,
-    run_names: str,
+    run_names: Union[List[str], str],
     config_name: str = "test",
     sorter: str = "kilosort2_5",
     use_existing_preprocessed_file: bool = False,
