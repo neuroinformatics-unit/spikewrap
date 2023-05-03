00b59f4f43081e05039b9ce3da70b03f756cfec3
Joseph Ziminski <jziminski@gpu-sr670-20.hpc.swc.ucl.ac.uk>
2023-05-03 15:43:57 +0000

    small changes

--- swc_ephys/examples/example_visualise.py
+++ swc_ephys/examples/example_visualise.py
@@ -9,7 +9,7 @@ from swc_ephys.pipeline.visualise import visualise
 base_path = r"N:\neuroinformatics\scratch\jziminski\ephys\test_data\steve_single_run"
 # r"/ceph/neuroinformatics/scratch/jziminski/ephys/test_data/steve_single_run"
 sub_name = "1110925"
-run_names = "1110925_test_shank1_g0"  # ["1119617_LSE1_shank12_cut", "
+run_names = "1110925_test_shank1"  # ["1119617_LSE1_shank12_cut", "
 # 1119617_pretest1_shank12_cut"]
 
 data = load_spikeglx_data(base_path, sub_name, run_names)
@@ -27,5 +27,5 @@ visualise(
         0,
         1,
     ),  # TODO: raise sensible error if this is longer than the recording time
-    run_number=2,
+    run_number=10,
 )
