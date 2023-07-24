d01a451001e8be588ac90e8c5c99ad8e5f5150d7
JoeZiminski <Joseph.J.Ziminski@gmail.com>
2023-07-24 14:24:38 +0000

    Tidy up, document and add type hints.

--- swc_ephys/pipeline/postprocess.py
+++ swc_ephys/pipeline/postprocess.py
@@ -1,22 +1,17 @@
-"""
-"""
 from __future__ import annotations
 
 import time
-from typing import TYPE_CHECKING, Dict, Optional, Union
+import warnings
+from pathlib import Path
+from typing import TYPE_CHECKING, Dict, Literal, Optional, Union
 
 import matplotlib.pyplot as plt
+import numpy
 import numpy as np
-from spikeinterface.core import compute_sparsity
-
-if TYPE_CHECKING:
-    from spikeinterface.core import BaseSorting
-
-from pathlib import Path
-
 import pandas as pd
 import spikeinterface as si
 from spikeinterface import curation
+from spikeinterface.core import compute_sparsity
 from spikeinterface.extractors import KiloSortSortingExtractor
 
 from ..configs.configs import get_configs
@@ -24,6 +19,25 @@ from ..data_classes.sorting import SortingData
 from ..pipeline.load_data import load_data_for_sorting
 from ..utils import utils
 from .waveform_compare import get_waveform_similarity
+
+if TYPE_CHECKING:
+    from spikeinterface import WaveformExtractor
+    from spikeinterface.core import BaseSorting
+
+MATRIX_BACKEND: Literal["numpy", "jax"]
+try:
+    MATRIX_BACKEND = "jax"
+except ImportError:
+    warnings.warn(
+        "The module Jax was not found. Waveform similarity matrix calculation"
+        "will be very slow. Use `pip install jax jaxlib` to install."
+    )
+    MATRIX_BACKEND = "numpy"
+
+
+# --------------------------------------------------------------------------------------
+# Run Postprocessing
+# --------------------------------------------------------------------------------------
 
 
 def run_postprocess(
@@ -52,8 +66,11 @@ def run_postprocess(
     verbose : bool
         If True, messages will be printed to console updating on the
         progress of preprocessing / sorting.
+
+    waveform_options: Dict
+        A dictionary containing options passed to SpikeInterface's
+        `extract_waveforms()` function as kwargs.
     """
-    # Load sorting
     if not isinstance(sorting_data, SortingData):
         sorting_data = load_data_for_sorting(
             Path(sorting_data),
@@ -88,15 +105,88 @@ def run_postprocess(
             f"Loading existing waveforms from: {sorting_data.waveforms_output_path}",
             verbose,
         )
-
         waveforms = si.load_waveforms(sorting_data.waveforms_output_path)
 
     save_plots_of_templates(sorting_data.waveforms_output_path, waveforms)
 
     # Perform postprocessing
+    save_quality_matrics(waveforms, sorting_data)
+    save_unit_locations(waveforms, sorting_data)
+    save_waveform_similarities(
+        sorting_data.waveforms_output_path, waveforms, MATRIX_BACKEND
+    )
+
+
+# Sorting Loader -----------------------------------------------------------------------
+
+
+def load_sorting_output(sorting_data: SortingData, sorter: str) -> BaseSorting:
+    """
+    Load the output of a sorting run as a SpikeInterface SortingExtractor
+    object.
+
+    It is assumed sorting is performed on concatenated runs. As such,
+    only a single sorting segment is expected, and error raised
+    if there are more.
+
+    Automatically remove empty units, which complicate downstream
+    processing. Also, remove excess spikes, which are caused by
+    strange behaviour of KS returning spike times that occur
+    outside the number of samples in a recording. This is required
+    for waveform extraction in Spikeinterface.
+
+    Parameters
+    ----------
+
+    sorting_data : SortingData
+        An swc_ephys SortingData object holding information about sorting
+
+    sorter : str
+        The sorter used (e.g. "kilosort2_5")
+
+    TODO
+    ----
+    In newer SpikeInterface version, `remove_empty_units()` is automatically
+    applied during `KiloSortSortingExtractor()`. We can simply use
+    this default rather than applying again here after pinning to newer
+    SI version. See  https://github.com/SpikeInterface/spikeinterface/issues/1760
+    """
+    if not sorting_data.sorter_run_output_path.is_dir():
+        raise FileNotFoundError(
+            f"{sorter} output was not found at "
+            f"{sorting_data.sorter_run_output_path}.\n"
+            f"Quality metrics will not be generated."
+        )
+
+    assert (
+        len(sorting_data) == 1
+    ), "unexpected number of entries in `sorting_data` dict."
+
+    recording = sorting_data[sorting_data.init_data_key]
+
+    sorting = KiloSortSortingExtractor(
+        folder_path=sorting_data.sorter_run_output_path,
+        keep_good_only=False,
+    )
+
+    sorting = sorting.remove_empty_units()
+    sorting_without_excess_spikes = curation.remove_excess_spikes(sorting, recording)
+
+    return sorting_without_excess_spikes
+
+
+# Helpers ------------------------------------------------------------------------------
+
+
+def save_quality_matrics(waveforms: WaveformExtractor, sorting_data: SortingData):
+    """ """
     quality_metrics = si.qualitymetrics.compute_quality_metrics(waveforms)
     quality_metrics.to_csv(sorting_data.quality_metrics_path)
+    utils.message_user(f"Quality metrics saved to {sorting_data.quality_metrics_path}")
 
+
+def save_unit_locations(waveforms: WaveformExtractor, sorting_data: SortingData):
+    """ """
     unit_locations = si.postprocessing.compute_unit_locations(
         waveforms, outputs="by_unit"
     )
@@ -105,15 +195,93 @@ def run_postprocess(
     )
     unit_locations_pandas.to_csv(sorting_data.unit_locations_path)
 
-    save_waveform_similarities(sorting_data.waveforms_output_path, waveforms)
-
-    utils.message_user(f"Quality metrics saved to {sorting_data.quality_metrics_path}")
     utils.message_user(f"Unit locations saved to {sorting_data.unit_locations_path}")
 
 
-def save_plots_of_templates(waveforms_output_path, waveforms):
-    """ """
+def save_waveform_similarities(
+    waveforms_output_path: Path,
+    waveforms: WaveformExtractor,
+    backend: Literal["jax", "numpy"],
+):
+    """
+    Save waveform similarity matrices as a csv file. This is a
+    num_waveform x num_waveform matrix holding the cosine similarity
+    between all waveforms for a unit.
+
+    The .csv file will contain header and index for the spike time of
+    each row.
+
+    Parameters
+    ---------
+
+    waveforms_output_path : Path
+        Pathlib object holding the output path where waveforms are saved
+        and /images will be written.
+
+    waveforms : WaveformExtractor
+        Spikeinterface WaveformExtractor object.
+
+    backend : Literal["jax", "numpy"]
+        The backend with which to run waveform comparisons. Jax is around 50
+        times faster in this case.
+    """
+    utils.message_user("Saving waveform similarity matrices...\n")
+
+    matrices_out_path = waveforms_output_path / "similarity_matrices"
+    matrices_out_path.mkdir(exist_ok=True)
+
     t = time.perf_counter()
+    for unit_id in waveforms.sorting.get_unit_ids():
+        sim_matrix, spike_times = get_waveform_similarity(waveforms, unit_id, backend)
+        sim_matrix_pd = pd.DataFrame(sim_matrix, columns=spike_times, index=spike_times)
+        sim_matrix_pd.to_csv(
+            matrices_out_path / f"waveform_similarity_unit_{unit_id}.csv"
+        )
+
+    utils.message_user(f"Waveform similarity matrices saved to: {matrices_out_path}")
+    utils.message_user(
+        f"Saving waveform similarity matrices took: {time.perf_counter() - t}"
+    )
+
+
+# --------------------------------------------------------------------------------------
+# Save imagines
+# --------------------------------------------------------------------------------------
+
+
+def save_plots_of_templates(waveforms_output_path: Path, waveforms: WaveformExtractor):
+    """
+    Save a plot of all templates in 'waveforms/images' folder. The plot
+    displays the template waveform are calculated in two ways
+        1) as the mean over the `unit_chan_idxs`, which are the channels
+           in which the template waveform signals are strongest.
+        2) as the channel in which the waveform signal is strongest (as
+           determined by the minimum value)
+
+    The purpose of these plots is to provide a quick overview of the
+    extract templates. Proper investigations on unit quality should
+    be done in Phy.
+
+    Parameters
+    ------
+
+    waveforms_output_path : Path
+        Pathlib object holding the output path where waveforms are saved
+        and /images will be written.
+
+    waveforms : WaveformExtractor
+        Spikeinterface WaveformExtractor object.
+
+    TODO
+    ----
+    This assumes the waveform are negative (argmin here, use of peak_sign="neg"
+    in compute_sparsity). Investigate and handle positive waveforms...
+
+    TODO: how to determine "neg", "pos", "both", how to decide the
+    peak_sign / radius when computing sparisty. Own function.
+    """
+    t = time.perf_counter()
+    utils.message_user("Saving template images...\n")
 
     fs = waveforms.sampling_frequency
     n_samples = waveforms.nsamples
@@ -123,24 +291,20 @@ def save_plots_of_templates(waveforms_output_path, waveforms):
         waveforms, peak_sign="neg", method="radius", radius_um=75
     )
 
+    y_label = "Voltage (uV)" if waveforms.return_scaled else "Voltage (unscaled)"
+
     for idx, unit_id in enumerate(waveforms.sorting.get_unit_ids()):
         unit_template = waveforms.get_template(unit_id)
-        unit_best_chan_idxs = sparsity.unit_id_to_channel_indices[unit_id]
+        unit_chan_idxs = sparsity.unit_id_to_channel_indices[unit_id]
 
-        # TODO: own function
-        # TODO: why are some templates all zeros?
-        # if unit_id in [33, "33"]:
-        #    breakpoint()
-        plt.plot(time_, np.mean(unit_template[:, unit_best_chan_idxs], axis=1))
+        plt.plot(time_, np.mean(unit_template[:, unit_chan_idxs], axis=1))
 
-        peak_chan_idx = np.argmin(
-            np.mean(unit_template[:, unit_best_chan_idxs], axis=0)
-        )
-        plt.plot(time_, unit_template[:, unit_best_chan_idxs[peak_chan_idx]])
+        peak_chan_idx = np.argmin(np.mean(unit_template[:, unit_chan_idxs], axis=0))
+        plt.plot(time_, unit_template[:, unit_chan_idxs[peak_chan_idx]])
 
         plt.legend(["max signal channel", "mean across best channels"])
         plt.xlabel("Time (ms)")
-        plt.ylabel("Voltage (uV)")
+        plt.ylabel(y_label)
         plt.title(f"Unit {unit_id} Template")
 
         output_folder = waveforms_output_path / "images"
@@ -148,51 +312,4 @@ def save_plots_of_templates(waveforms_output_path, waveforms):
         plt.savefig(waveforms_output_path / "images" / f"unit_{unit_id}.png")
         plt.clf()
 
-    print(f"Saving plots of templates took: {time.perf_counter() - t}")
-
-
-def load_sorting_output(sorting_data: SortingData, sorter: str) -> BaseSorting:
-    """
-    Load the output of a sorting run.
-
-    TODO: understand remove_excess_spikes.
-    """
-    if not sorting_data.sorter_run_output_path.is_dir():
-        raise FileNotFoundError(
-            f"{sorter} output was not found at "
-            f"{sorting_data.sorter_run_output_path}.\n"
-            f"Quality metrics were not generated."
-        )
-
-    assert len(sorting_data) == 1, (
-        "unexpected number of entries in " "`sorting_data` dict."
-    )
-
-    recording = sorting_data[sorting_data.init_data_key]
-
-    sorting = KiloSortSortingExtractor(
-        folder_path=sorting_data.sorter_run_output_path,
-        keep_good_only=False,
-    )
-
-    # TODO: use upcoming SI option, see
-    #  https://github.com/SpikeInterface/spikeinterface/issues/1760
-    sorting = sorting.remove_empty_units()
-    sorting_without_excess_spikes = curation.remove_excess_spikes(sorting, recording)
-
-    return sorting_without_excess_spikes
-
-
-def save_waveform_similarities(waveforms_output_path, waveforms):
-    out_path = waveforms_output_path / "similarity_matricies"
-    out_path.mkdir(exist_ok=True)
-
-    time.perf_counter()
-    for unit_id in waveforms.sorting.get_unit_ids():
-        sim_matrix, spike_times = get_waveform_similarity(waveforms, unit_id, "jax")
-
-        sim_matrix_pd = pd.DataFrame(sim_matrix, columns=spike_times, index=spike_times)
-        sim_matrix_pd.to_csv(out_path / f"waveform_similarity_unit_{unit_id}.csv")
-
-    # TODO: use message utils
-    print(f"Saving waveform similarities took: {time.perf_counter()} - t")
+    utils.message_user(f"Saving plots of templates took: {time.perf_counter() - t}")
--- swc_ephys/pipeline/waveform_compare.py
+++ swc_ephys/pipeline/waveform_compare.py
@@ -1,4 +1,6 @@
-from typing import Literal
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal
 
 import jax.numpy as jnp
 import numpy as np
@@ -6,76 +8,175 @@ from jax import jit, vmap
 from jax.lax import scan
 from spikeinterface.core import compute_sparsity
 
+from ..utils import utils
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+    from spikeinterface import WaveformExtractor
+
 
 def get_waveform_similarity(
-    waveforms, unit_id, backend: Literal["numpy", "jax"] = "numpy"
+    waveforms: WaveformExtractor, unit_id: int, backend: Literal["numpy", "jax"] = "jax"
 ):
-    data = waveforms.get_waveforms(unit_id=unit_id)
+    """
+    Calculate a num_waveform x num_waveform matrix containing
+    cosine similarity between all waveforms for a unit. The waveforms
+    are saved as 3d array (num_waveforms x num_samples x num_channels) by
+    SpikeInterface, and waveforms are averaged across the channels
+    where the signal is strongest (see `get_times_of_waveform_spikes()`)
+    prior to cosine similarity calculation.
 
-    # TODO: check with SI these assumptions are correct
-    # Own function
+    waveforms : WaveformExtractor
+        Spikeinterface WaveformExtractor object.
+
+    unit_id : int
+        ID of the unit to calculate the similarity matrix for.
+
+    backend : Literal["jax", "numpy"]
+        The backend with which to run waveform comparisons. Jax is around 50
+        times faster in this case.
+    """
+    waveforms_data = waveforms.get_waveforms(unit_id=unit_id)
+
+    selected_waveform_peak_times = get_times_of_waveform_spikes(waveforms, unit_id)
+
+    assert (
+        waveforms_data.shape[0] == selected_waveform_peak_times.size
+    ), "Number of waveform peak times does not match number of extracted waveforms."
+
+    if waveforms_data.shape[0] == 1:
+        utils.message_user("Skipping {unit_id} as one waveform detected for this unit.")
+        return np.nan, selected_waveform_peak_times
+
+    sparsity = compute_sparsity(
+        waveforms, peak_sign="neg", method="radius", radius_um=75
+    )
+    unit_chan_idxs = sparsity.unit_id_to_channel_indices[unit_id]
+
+    if backend == "numpy":
+        similarity_matrix = calculate_similarity_numpy(waveforms_data, unit_chan_idxs)
+    elif backend == "jax":
+        similarity_matrix = calculate_similarity_jax(waveforms_data, unit_chan_idxs)
+    else:
+        raise ValueError("`backend` must be: 'jax' or 'numpy'")
+
+    return similarity_matrix, selected_waveform_peak_times
+
+
+def get_times_of_waveform_spikes(waveforms, unit_id):
+    """
+    Extract the peak times of the waveforms that were randomly sampled.
+    SpikeInterface WaveformExtractor randomly samples a set of spikes
+    from all spikes in a unit (default 500) to reduce computation time
+    and memory footprint. As such, the index of the sampled waveforms
+    is used in the list of all spike times to find the sampled spike times.
+
+    TODO
+    ----
+    Check manually that the correct spike times are extracted, as compared with Phy.
+    """
     select_waveform_tuples = waveforms.get_sampled_indices(unit_id)
     select_waveform_idxs, seg_idxs = zip(*select_waveform_tuples)
-    assert np.all(np.array(seg_idxs) == 0), "multi-segment waveforms not tested."
+    assert np.all(np.array(seg_idxs) == 0), "Multi-segment waveforms not tested."
 
     all_waveform_peak_idxs = waveforms.sorting.get_unit_spike_train(unit_id)
     selected_waveform_idxs = all_waveform_peak_idxs[np.array(select_waveform_idxs)]
     selected_waveform_peak_times = selected_waveform_idxs / waveforms.sampling_frequency
 
-    if data.shape[0] == 1:
-        print("only one cluster")  # TODO: better message, use utils
-        return np.nan, selected_waveform_peak_times
+    return selected_waveform_peak_times
 
-    # TODO: how to determine "neg", "pos", "both", how to decide the
-    #  peak_sign / radius when computing sparisty. Own function.
-    sparsity = compute_sparsity(
-        waveforms, peak_sign="neg", method="radius", radius_um=75
-    )
-    unit_best_chan_idxs = sparsity.unit_id_to_channel_indices[unit_id]
 
-    if backend == "numpy":
-        sim = calculate_similarity_numpy(data, unit_best_chan_idxs)
-    elif backend == "jax":
-        sim = calculate_similarity_jax(data, unit_best_chan_idxs)
-
-    return sim, selected_waveform_peak_times
+# --------------------------------------------------------------------------------------
+# Similarity Matrix Calculators
+# --------------------------------------------------------------------------------------
 
 
 @jit
-def calculate_similarity_jax(data, unit_best_chan_idxs):
-    # This actually duplicates the upper and lower triangular,
-    # but is so fast it doesn't really matter. vmapping occurs
-    # across the entire axis so not sure it can be re-configured.
-    # Can think a bit more about this though.
-    data_mean = jnp.mean(data[:, :, unit_best_chan_idxs], axis=2)
+def calculate_similarity_jax(waveforms_data, unit_chan_idxs):
+    """
+    Speed up calculation of similarity matrix using Jax. First, average
+    the waveforms across the best channels (where the waveform signal
+    is strongest) for the unit. Then, define a vmapped function that
+    calculates the cosine similarity between a waveform and every
+    other waveform. Finally, use scan to replicate a for-loop
+    over every waveform.
+
+    Note that this redundantly calculates the upper and lower traingular
+    of the similarity matrix, but this is necessary to take advantage of vmap
+    speed-ups.
+
+    Parameters
+    ----------
+    waveforms_data : Jax.Array
+        A num_waveforms x num_samples x num_channels array containing
+        data of all waveforms.
+
+    unit_chan_idxs : Jax.Array
+        Array of channel indices where the unit signal is strongest,
+        extracted with SpikeInterface `get_sampled_indices()`.
+
+    Returns
+    -------
+    similarity_matrix : Jax.Array
+        num_waveform x num_waveform matrix of cosine similarities
+        for the unit.
+    """
+    averaged_waveforms = jnp.mean(waveforms_data[:, :, unit_chan_idxs], axis=2)
 
     def func(carry, i):
-        y = data_mean[i, :]
-        sim_row = vmap(
+        y = averaged_waveforms[i, :]
+
+        sim_matrix_row = vmap(
             lambda x: jnp.dot(x, y.T) / (jnp.linalg.norm(x) * jnp.linalg.norm(y)),
             in_axes=0,
-        )(data_mean)
-        return carry, sim_row
+        )(averaged_waveforms)
 
-    # TOOD: is this stylistically okay? because carry does not change.
-    # Using is to hack a loop.
-    return scan(func, data_mean, np.arange(data_mean.shape[0]))[1]
+        return carry, sim_matrix_row
+
+    similarity_matrix = scan(
+        func, averaged_waveforms, np.arange(averaged_waveforms.shape[0])
+    )[1]
+
+    return similarity_matrix
 
 
-def calculate_similarity_numpy(data, unit_best_chan_idxs):  # TODO: variable naming
-    """ """
-    num_spikes = data.shape[0]
-    data_mean = np.mean(data[:, :, unit_best_chan_idxs], axis=2)
+def calculate_similarity_numpy(waveforms_data: NDArray, unit_chan_idxs: NDArray):
+    """
+    The similarity matrix is calculated by filling the
+    upper triangular only then copying to the lower triangular.
 
-    sim = np.zeros((num_spikes, num_spikes))
+    Parameters
+    ----------
+
+    waveforms_data : NDArray
+    A num_waveforms x num_samples x num_channels array containing
+    data of all waveforms.
+
+    unit_chan_idxs : NDArray
+        Array of channel indices where the unit signal is strongest,
+        extracted with SpikeInterface `get_sampled_indices()`.
+
+    Returns
+    -------
+
+    similarity_matrix : NDArray
+    num_waveform x num_waveform matrix of cosine similarities
+    for the unit.
+    """
+    num_spikes = waveforms_data.shape[0]
+    averaged_waveforms = np.mean(waveforms_data[:, :, unit_chan_idxs], axis=2)
+
+    similarity_matrix = np.zeros((num_spikes, num_spikes))
     for j in range(num_spikes):
         for i in range(j + 1):
-            x = data_mean[i, :]
-            y = data_mean[j, :]
+            x = averaged_waveforms[i, :]
+            y = averaged_waveforms[j, :]
 
-            sim[i, j] = np.dot(x, y.T) / (np.linalg.norm(x) * np.linalg.norm(y))
+            similarity_matrix[i, j] = np.dot(x, y.T) / (
+                np.linalg.norm(x) * np.linalg.norm(y)
+            )
 
-    i_lower = np.tril_indices(sim.shape[0], -1)
-    sim[i_lower] = sim.T[i_lower]
+    i_lower = np.tril_indices(similarity_matrix.shape[0], -1)
+    similarity_matrix[i_lower] = similarity_matrix.T[i_lower]
 
-    return sim
+    return similarity_matrix
