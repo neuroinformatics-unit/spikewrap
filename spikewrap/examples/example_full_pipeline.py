d79b4dc5b282b7579291b34b668c1faad2b081fa
Joe Ziminski <55797454+JoeZiminski@users.noreply.github.com>
2023-08-04 00:13:38 +0100

    Add checks option, check docker is running before sorting (if required).

--- spikewrap/examples/example_full_pipeline.py
+++ spikewrap/examples/example_full_pipeline.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from spikewrap.pipeline.full_pipeline import run_full_pipeline
 
 base_path = Path(
-    "/ceph/neuroinformatics/neuroinformatics/scratch/jziminski/ephys/test_data/steve_multi_run/1119617/time-short"
+    r"C:\data\ephys\test_data\steve_multi_run\1119617\time-short"
+    # "/ceph/neuroinformatics/neuroinformatics/scratch/jziminski/ephys/test_data/steve_multi_run/1119617/time-short"
 )
-#    r"C:\data\ephys\test_data\steve_multi_run\1119617\time-short"
 sub_name = "1119617"
 run_names = [
     "1119617_LSE1_shank12",
@@ -14,7 +14,7 @@ run_names = [
 ]
 
 config_name = "default"
-sorter = "mountainsort5"  #  "kilosort2_5"  # "spykingcircus" # mountainsort5
+sorter = "kilosort2_5"  #  "kilosort2_5"  # "spykingcircus" # mountainsort5
 
 if __name__ == "__main__":
     run_full_pipeline(
