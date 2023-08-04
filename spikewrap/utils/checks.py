f4fdab0d6b9f54c3bee64da05ce1a49f003a8b64
Joe Ziminski <55797454+JoeZiminski@users.noreply.github.com>
2023-08-04 10:40:47 +0100

    Add environment checks (#67)
    * Add checks option, check docker is running before sorting (if required).
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix linting.
    
    * Add checks option, check docker is running before sorting (if required).
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix linting.
    
    * Bug fixes on Linux.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Doc and final tidy ups.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ spikewrap/utils/checks.py
@@ -0,0 +1,101 @@
+import platform
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+import toml
+
+from spikewrap.utils import utils
+
+
+def check_environment() -> None:
+    """ """
+    _check_virtual_machine()
+    _check_cuda()
+    _check_pip_dependencies()
+
+
+def _check_virtual_machine() -> bool:
+    """"""
+    if platform.system() == "Linux":
+        has_vm = _system_call_sucess("singularity version")
+        name = "Singularity"
+        link = (
+            "https://docs.sylabs.io/guides/main/user-guide/quick_start.html#quick"
+            "-installation-steps"
+        )
+    else:
+        has_vm = _system_call_sucess("docker -v")
+        name = "Docker"
+        if platform.system() == "Windows":
+            link = "https://docs.docker.com/desktop/install/windows-install/"
+        else:
+            link = "https://docs.docker.com/desktop/install/mac-install/"
+
+    if has_vm:
+        utils.message_user(
+            f"{name} is installed. Sorters such as Kilosort that\n"
+            f"cannot be run in native Python will run in a virtual machine."
+        )
+        return True
+
+    utils.message_user(
+        f"{name} is not installed. Sorters such as Kilosort that\n"
+        f"cannot be run in native Python are not available. To install\n"
+        f"{name}, see: {link}"
+    )
+    return False
+
+
+def docker_desktop_is_running():
+    return _system_call_sucess("docker ps")
+
+
+def _check_cuda() -> None:
+    """"""
+    if _system_call_sucess("nvidia-smi"):
+        utils.message_user("NVIDIA GPU drivers detected on the system.")
+    else:
+        utils.message_user(
+            "NVIDIA GPU drivers not detected. Sorters that require\n"
+            "GPU such as Kilosort will not be able to run."
+        )
+
+
+def _check_pip_dependencies() -> None:
+    """"""
+    utils.message_user("Checking Python dependencies...")
+    pyproject_path = (
+        Path(sys.modules["spikewrap"].__path__[0]).parent / "pyproject.toml"
+    )
+    pyproject_toml = toml.load(pyproject_path.as_posix())
+
+    pip_list = subprocess.run(
+        "pip list",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    ).stdout.decode()
+
+    all_deps_installed = True
+    for dep in pyproject_toml["project"]["dependencies"]:
+        dep_name = re.split("<|>|=", dep)[0]
+
+        if dep_name not in pip_list:
+            all_deps_installed = False
+            utils.message_user(
+                f"The dependency {dep_name} was not found in the current environment using pip.\n"
+                f"Ensure all dependencies are installed by reinstalling SpikeWrap."
+            )
+    if all_deps_installed:
+        utils.message_user("All python dependencies are installed.")
+
+
+def _system_call_sucess(command: str) -> bool:
+    return (
+        subprocess.run(
+            command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        ).returncode
+        == 0
+    )
