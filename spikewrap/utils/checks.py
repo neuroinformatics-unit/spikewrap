eb05c973e3ad0b1c96cef5bd85d04e3ef65df2fb
JoeZiminski <Joseph.J.Ziminski@gmail.com>
2023-08-04 10:55:12 +0100

    Add docs to checks.py

--- spikewrap/utils/checks.py
+++ spikewrap/utils/checks.py
@@ -10,23 +10,36 @@ from spikewrap.utils import utils
 
 
 def check_environment() -> None:
-    """ """
+    """
+    Check that a virtual machine manager is installed (Singularity for Linux,
+    Docker for macOS or Windows). Perform a minimal check that cuda drivers
+    are installed. Finally, check all dependencies listed in pyproject.toml
+    are installed in the environment.
+    """
     _check_virtual_machine()
     _check_cuda()
     _check_pip_dependencies()
 
 
 def _check_virtual_machine() -> bool:
-    """"""
+    """
+    Check that a virtual machine manager is installed on the
+    system (singularity for Linux, Docker otherwise). Note that
+    sorters that can run natively in python
+    do not require virtual machines, only Kilosort < 4 at present.
+
+    If the virtual machine manager is not found on the system, print
+    a link to installation instructions.
+    """
     if platform.system() == "Linux":
-        has_vm = _system_call_sucess("singularity version")
+        has_vm = _system_call_success("singularity version")
         name = "Singularity"
         link = (
             "https://docs.sylabs.io/guides/main/user-guide/quick_start.html#quick"
             "-installation-steps"
         )
     else:
-        has_vm = _system_call_sucess("docker -v")
+        has_vm = _system_call_success("docker -v")
         name = "Docker"
         if platform.system() == "Windows":
             link = "https://docs.docker.com/desktop/install/windows-install/"
@@ -49,22 +62,32 @@ def _check_virtual_machine() -> bool:
 
 
 def docker_desktop_is_running():
-    return _system_call_sucess("docker ps")
+    """
+    Note "docker -v" shows if docker is installed but not necessarily
+    running. "docker ps" requires docker to be running, which can
+    be achieved by opening Docker Desktop.
+    """
+    return _system_call_success("docker ps")
 
 
 def _check_cuda() -> None:
-    """"""
-    if _system_call_sucess("nvidia-smi"):
+    """
+    Perform a very basic check that NVIDIA drivers are installed. This
+    however does not ensure GPU processing will work without error.
+    """
+    if _system_call_success("nvidia-smi"):
         utils.message_user("NVIDIA GPU drivers detected on the system.")
     else:
         utils.message_user(
             "NVIDIA GPU drivers not detected. Sorters that require\n"
-            "GPU such as Kilosort will not be able to run."
-        )
+            "NVIDIA GPU such as Kilosort will not be able to run."
 
 
 def _check_pip_dependencies() -> None:
-    """"""
+    """
+    Perform a confidence check that all dependencies listed
+    in the pyproject.toml are installed in the current environment.
+    """
     utils.message_user("Checking Python dependencies...")
     pyproject_path = (
         Path(sys.modules["spikewrap"].__path__[0]).parent / "pyproject.toml"
