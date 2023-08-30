dbfad2563c468a753d3d998dbb809587bce4ae95
Joe Ziminski <55797454+JoeZiminski@users.noreply.github.com>
2023-08-31 00:16:51 +0100

    Add key imports to __init__.

--- spikewrap/__init__.py
+++ spikewrap/__init__.py
@@ -5,3 +5,20 @@ try:
 except PackageNotFoundError:
     # package is not installed
     pass
+
+from .pipeline.full_pipeline import run_full_pipeline
+from .pipeline.preprocess import run_preprocess
+from .pipeline.sort import run_sorting
+from .pipeline.postprocess import run_postprocess
+
+from .utils.checks import check_environment
+from .utils.slurm import run_interactive_slurm
+
+__all__ = [
+    "run_full_pipeline",
+    "run_preprocess",
+    "run_sorting",
+    "run_postprocess",
+    "check_environment",
+    "run_interactive_slurm",
+]
