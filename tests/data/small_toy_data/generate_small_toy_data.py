a05eb73249946f737e4e41306c8bdb20c20872c1
JoeZiminski <Joseph.j.ziminski@gmail.com>
2023-12-15 11:59:16 +0000

    Tidy up generate_small_toy_data

--- tests/data/small_toy_data/generate_small_toy_data.py
+++ tests/data/small_toy_data/generate_small_toy_data.py
@@ -32,9 +32,10 @@ for ses in sessions_and_runs.keys():
         recording.set_property("group", four_shank_groupings)
         recording.set_property("inter_sample_shift", np.arange(16) * 0.0001)
 
+        # for consistency with spikeglx dataset TODO this is a hack
         recording._main_ids = np.array(
             [f"imec0.ap#AP{i}" for i in range(num_channels)]
-        )  # for consistency with spikeglx dataset TODO this is a hack
+        )
 
         recording = spre.scale(recording, gain=50, offset=20)
 
@@ -45,7 +46,7 @@ for ses in sessions_and_runs.keys():
         recording.save(folder=output_path, chunk_size=1000000)
 
 # It is really important for testing that all saved recordings are different.
-# This is achevied by scaling above. Check here that indeed all recording
+# This is achieved by scaling above. Check here that indeed all recording
 # data is different.  This should always be the case becase toy_example
 # is random.
 all_data = [rec.get_traces() for rec in track_recordings]
@@ -60,9 +61,3 @@ for i in range(len(track_recordings)):
         rtol=0,
         atol=1,
     )
-
-
-# shifted_recording = spre.phase_shift(recording)
-# filtered_recording = spre.bandpass_filter(recording)
-# referenced_recording = spre.common_reference(filtered_recording)
-# run_sorter("mountainsort5", referenced_recording, remove_existing_folder=True, whiten=False)
