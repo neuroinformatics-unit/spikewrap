e2ba6d4879c2245c5a8da4bd816218079da11fe7
JoeZiminski <Joseph.j.ziminski@gmail.com>
2025-01-21 16:43:13 +0000

    First commit.

---
+++ spikewrap/__init__.py
@@ -0,0 +1,24 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("swc_epys")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+from .pipeline.full_pipeline import run_full_pipeline
+from .pipeline.preprocess import _preprocess_and_save_all_runs
+from .pipeline.sort import run_sorting
+from .pipeline.postprocess import run_postprocess
+
+from .utils.checks import check_environment
+from .utils.slurm import run_interactive_slurm
+
+__all__ = [
+    "run_full_pipeline",
+    "_preprocess_and_save_all_runs",
+    "run_sorting",
+    "run_postprocess",
+    "check_environment",
+    "run_interactive_slurm",
+]
