7829f28c19f6c680bb594cd01a528c85c7f80214
JoeZiminski <joseph.j.ziminski@gmail.com>
2025-03-10 14:01:44 +0000

    Tidy up example.

--- spikewrap/examples/example_session.py
+++ spikewrap/examples/example_session.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
         file_format="spikeglx",
     )
 
-    # TODO: document well the class lifespan
     session.preprocess(
-        configs="neuropixels+kilosort2_5",
+        configs="neuropixels+mountainsort5",
         per_shank=True,
         concat_runs=True,
     )
@@ -24,26 +23,11 @@ if __name__ == "__main__":
         overwrite=True, n_jobs=1, slurm=False, chunk_duration_s=0.1
     )
 
-    # 1) figure out what will happen for all per_shank, concat_runs combinations
-    # 2) figure out slurm
-    # 3) figure out overwrite
-    # 4) figure out run_method
-
-    # TODO: properly test runs and stuff
-    # 1) make a test script for singularity
-    # 2) and docker
-    # 3) and matlab
-    # 4) and ofc local
-    # TODO: check there is a warning when per_shank is on for prepro and off for sorting. maybe raise?
     session.sort(
         configs="neuropixels+mountainsort5",
-        run_sorter_method="local",  # "local", "singularity", "docker" or path to MATLAB install (check for mex files!)
+        run_sorter_method="local",
         per_shank=False,
         concat_runs=False,
         overwrite=True,
         slurm=False,
     )
-
-    """
-8)  handle existing preprocessing, loading into preprocessing etc.
-    """
