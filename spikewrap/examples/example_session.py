471cb2eb279a20d3682403fdec3bf8b7f951dda1
JoeZiminski <Joseph.j.ziminski@gmail.com>
2025-03-11 12:46:18 +0000

    Fix example file.

--- spikewrap/examples/example_session.py
+++ spikewrap/examples/example_session.py
@@ -4,37 +4,30 @@ if __name__ == "__main__":
 
     # Make up a probe for this recording
     session = sw.Session(
-        subject_path=r"X:\neuroinformatics\scratch\jziminski\ephys\kkouk\crabs\rawdata\sub-002_id-KK07",
-        session_name="ses-01_20250123",
+        subject_path=sw.get_example_data_path("spikeglx") / "rawdata" / "sub-001",
+        session_name="ses-001",
         file_format="spikeglx",
     )
 
-    session.load_raw_data()
+    session.preprocess(
+        configs="neuropixels+mountainsort5",
+        per_shank=True,
+        concat_runs=True,
+    )
 
-    if False:
-        session.preprocess(
-            configs="neuropixels+mountainsort5",
-            per_shank=True,
-            concat_runs=True,
-        )
+    session.plot_preprocessed(
+        run_idx="all", mode="map", show_channel_ids=False, show=True, figsize=(12, 8)
+    )
 
-        session.plot_preprocessed(
-            run_idx="all",
-            mode="map",
-            show_channel_ids=False,
-            show=True,
-            figsize=(12, 8),
-        )
+    session.save_preprocessed(
+        overwrite=True, n_jobs=1, slurm=False, chunk_duration_s=0.1
+    )
 
-        session.save_preprocessed(
-            overwrite=True, n_jobs=1, slurm=False, chunk_duration_s=0.1
-        )
-
-        session.sort(
-            configs="neuropixels+mountainsort5",
-            run_sorter_method="local",
-            per_shank=False,
-            concat_runs=False,
-            overwrite=True,
-            slurm=False,
-        )
+    session.sort(
+        configs="neuropixels+mountainsort5",
+        run_sorter_method="local",
+        per_shank=False,
+        concat_runs=False,
+        overwrite=True,
+        slurm=False,
+    )
