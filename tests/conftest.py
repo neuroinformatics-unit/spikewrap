de289f8aecf08faebeeca52cecff1783f7466f87
JoeZiminski <Joseph.j.ziminski@gmail.com>
2026-03-12 17:42:11 +0000

    Fix tcl error for python 3.12 and windows issue.

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import sys
+
+# There is a bug in Python 3.12/tcl/windows that leads to intermittent errors in tests, as described below:
+# https://stackoverflow.com/questions/71443540/intermittent-pytest-failures-complaining-about-missing-tcl-files-even-though-the
+# A quick workaround is to use a non-tkinter backend for the tests in this case.
+if sys.version_info >= (3, 12):
+    import matplotlib
+
+    matplotlib.use("Agg")
